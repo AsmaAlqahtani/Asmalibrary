ad3efcace8df490b374af45eb448e86dee95a9c1
AsmaAlqahtani <63420555+AsmaAlqahtani@users.noreply.github.com>
2021-01-07 22:02:57 +0300

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+Name1 = input(print ("Enter your name"))
+
+
+print("Welcome" +Name1)
+
+
+# In[16]:
+
+
+Num1 = float (input("Enter the first number"))
+Num2 = float (input("Enter the second number"))
+
+print (Num1 + Num2)
+
